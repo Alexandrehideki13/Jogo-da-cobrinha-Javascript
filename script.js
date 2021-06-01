61a61ff7525cc0df0c5d560c7b5951f63613d331
Alexandre Hideki <alexandrejilek@gmail.com>
2021-06-01 17:26:37 -0300

    Adicionando a cabeça da cobra

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
-    y: 8 * box
-}
+    y: 8 * box,
+};
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
-}
+    y: Math.floor(Math.random() * 15 + 1) * box,
+};
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,8 +19,13 @@ function criarBG() {
 
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        if (i == 0) {
+            context.fillStyle = "rgb(171, 104, 21)";
+            context.fillRect(snake[i].x, snake[i].y, box, box);
+        } else {
+            context.fillStyle = "green";
+            context.fillRect(snake[i].x, snake[i].y, box, box);
+        }
     }
 }
 
@@ -29,7 +34,7 @@ function drawFood() {
     context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
+document.addEventListener("keydown", update);
 
 function update(event) {
     if (event.keyCode == 37 && direction != "right") direction = "left";
@@ -47,7 +52,7 @@ function iniciarJogo() {
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
-            alert('Game over ;-;');
+            alert("Game over ;-;");
         }
     }
 
@@ -72,8 +77,8 @@ function iniciarJogo() {
 
     let newHead = {
         x: snakeX,
-        y: snakeY
-    }
+        y: snakeY,
+    };
 
     snake.unshift(newHead);
 }
